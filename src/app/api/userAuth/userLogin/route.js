e32a4e38724193c4078e748e9b3f05677c9fa617
ShakhawatHossainShopon <shoponhossain32186@gmail.com>
2025-03-16 23:48:50 +0600

    debugging

--- src/app/api/userAuth/userLogin/route.js
+++ src/app/api/userAuth/userLogin/route.js
@@ -1,36 +1,13 @@
-
 import { connectToDB } from "@/lib/mongodb";
-
 import { NextResponse } from "next/server";
 
-const secretKey = process.env.JWT_SECRET;
-
-if (!secretKey) {
-  throw new Error("JWT_SECRET is not defined in environment variables.");
-}
-
-export async function POST(req) {
+export async function GET() {
   try {
-    const { email, password } = await req.json(); // Parse request body
-    if (!email || !password) {
-      return NextResponse.json({ error: "Email and password are required" }, { status: 400 });
-    }
-    // Connect to MongoDB
-    const db = await connectToDB();
-
-    if (!db) {
-      return NextResponse.json({ error: "Failed to connect to MongoDB" }, { status: 500 });
-    }
-    // Find the user in the database
-    const user = await db.collection("users").findOne({ email });
-    if (!user) {
-      return NextResponse.json({ error: "Invalid email or password" }, { status: 401 });
-    }
-
+    await connectToDB(); // Connect to MongoDB
 
     return NextResponse.json({ message: "MongoDB connected successfully!" });
   } catch (error) {
-    console.error("Error in login handler:", error);
-    return NextResponse.json({ error: "Failed to login" }, { status: 500 });
+    console.error("MongoDB Connection Error:", error);
+    return NextResponse.json({ error: "Failed to connect to MongoDB" }, { status: 500 });
   }
 }
