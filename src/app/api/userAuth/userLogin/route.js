5208b9cca952ffb818485ed2d0e1708b9840ee0c
ShakhawatHossainShopon <shoponhossain32186@gmail.com>
2025-03-16 23:20:28 +0600

    bug fixing

--- src/app/api/userAuth/userLogin/route.js
+++ src/app/api/userAuth/userLogin/route.js
@@ -8,37 +8,19 @@ const secretKey = process.env.JWT_SECRET;
 
 export async function POST(req) {
   try {
-    const { email, password } = await req.json();
-    if (!secretKey) {
-      return NextResponse.json({ error: "Jwt Missing" }, { status: 403 });
-    }
-    connectToDB();
-    const user = await User.findOne({ email });
+    // Check MongoDB connection status
+    const connectionStatus = await connectToDB();
 
-    if (!user) {
-      return NextResponse.json(
-        { error: "Invalid email or password" },
-        { status: 400 }
-      );
-    }
-    if (user.password !== password) {
-      return NextResponse.json(
-        { error: "Invalid email or password" },
-        { status: 400 }
-      );
-    }
-    const role = "User";
-    const token = jwt.sign(
-      { userId: user._id, shopId: user.shopId, role: role }, // Include userId and shopId in the payload
-      secretKey
-    );
+    // If connection is successful
+    if (connectionStatus.success) {
+      console.log(connectionStatus.message); // MongoDB is connected
 
-    // Step 6: Return the JWT token in the response
-    const res = NextResponse.json({ message: "Login successful" });
-    res.cookies.set("userToken", token, {
-      httpOnly: true,
-    });
-    return res;
+      // Proceed with your login or other logic here
+      return NextResponse.json({ message: "MongoDB connected successfully!" });
+    } else {
+      // If failed to connect
+      return NextResponse.json({ error: connectionStatus.message }, { status: 403 });
+    }
   } catch (erorr) {
     return NextResponse.json({ error: "Failed to login" }, { status: 500 });
   }
